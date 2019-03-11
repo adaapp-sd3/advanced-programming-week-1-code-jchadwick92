6ef8b884b3dce335dfa06ddd6add64f31c84220b
Joe Chadwick <joe.chadwick@gamesys.co.uk>
2019-03-11 14:23:05 +0000

    Task component with props

---
+++ workshop-one/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Task from './tasks/Task'
+
+const dummyTaskData = [
+  {
+    title: "Slow down",
+    description: "When leading from the front, you cannot go too slow"
+  },
+  {
+    title: "Provide stretch exercise",
+    description: "Ensure all students have meaningful, challenging work to do."
+  },
+  {
+    title: "Provide actual stretch exercise",
+    description: "It is important whilst coding to stretch."
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <section>
+          <Task title="A" description="1" />
+          <Task title="B" description="2"  />
+          <Task title="C" description="3"  />
+          <Task title="D" description="4"  />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ workshop-one/src/tasks/Task.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Task extends Component {
+  render() {
+    return (
+      <div className="Task">
+        <h2>{this.props.title}</h2>
+        <p>{this.props.description}</p>
+      </div>
+    );
+  }
+}
+
+export default Task;
